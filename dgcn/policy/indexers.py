823817c6ed6304559a6bb0aa9807d43d5f788921
TsungWei Hu <marr.tw@gmail.com>
2015-03-27 16:30:12 +0800

    Add Indexes for Map View Timeline.

--- dgcn/policy/indexers.py
+++ dgcn/policy/indexers.py
@@ -1,8 +1,25 @@
 from plone.indexer.decorator import indexer
 from dgcn.content.photo import IPhoto
+from DateTime import DateTime
 
 
 @indexer(IPhoto)
 def photographer(obj):
     return obj.photographer
+
+@indexer(IPhoto)
+def year(obj):
+    return obj.year
+
+@indexer(IPhoto)
+def start(obj):
+    if obj.startDate is None:
+        return None
+    return DateTime(obj.startDate.isoformat())
+
+@indexer(IPhoto)
+def end(obj):
+    if obj.endDate is None:
+        return None
+    return DateTime(obj.endDate.isoformat())
 
